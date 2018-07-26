4e087eba976374d991736fb51e1227f4eb510d39
serhii <californication715@gmail.com>
2018-07-26 16:33:56 +0300

    /add clean imports

--- backend/src/main/java/com/javabrazzers/controller/MainController.java
+++ backend/src/main/java/com/javabrazzers/controller/MainController.java
@@ -1,17 +1,12 @@
 package com.javabrazzers.controller;
 
-import com.javabrazzers.domain.User;
-import com.javabrazzers.repository.UserRepository;
-import com.javabrazzers.service.UserService;
-import jdk.internal.jline.internal.Log;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.time.LocalDateTime;
-import java.util.Map;
 
 @RestController
 @RequestMapping("/index")
