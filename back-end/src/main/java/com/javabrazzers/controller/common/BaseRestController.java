30eb2e633b97bd4d4cf2b7ac23ea0837393f875c
moreyl <yan.alferov@yahoo.com>
2018-07-29 22:19:55 +0300

    update

---
+++ back-end/src/main/java/com/javabrazzers/controller/common/BaseRestController.java
@@ -0,0 +1,10 @@
+package com.javabrazzers.controller.common;
+
+import com.javabrazzers.domain.BaseEntity;
+import org.springframework.http.ResponseEntity;
+
+public interface BaseRestController <E extends BaseEntity> {
+    ResponseEntity<E> getById(Long id);
+
+
+}
