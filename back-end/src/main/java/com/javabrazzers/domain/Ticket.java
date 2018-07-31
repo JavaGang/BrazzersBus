8a00dce6e77a196841441004aa944cc7a5431afb
iBublig <theaganrmnon@gmail.com>
2018-08-01 01:15:06 +0400

    #28: Modified Ticket, Repo and Service.

--- back-end/src/main/java/com/javabrazzers/domain/Ticket.java
+++ back-end/src/main/java/com/javabrazzers/domain/Ticket.java
@@ -1,8 +1,21 @@
 package com.javabrazzers.domain;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.Entity;
+import javax.persistence.Table;
 import java.time.LocalDateTime;
 
-public class Ticket {
+@Getter
+@Setter
+@Entity
+@Table(name = "ticket_tbl")
+@AllArgsConstructor
+@NoArgsConstructor
+public class Ticket extends BaseEntity {
 
 	private LocalDateTime purchase_date;
 	private int ticketNumber;
