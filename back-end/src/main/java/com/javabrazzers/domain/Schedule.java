dd93ca43d3ab1adc6f5b5629d8b3bbf3998d792b
iBublig <theaganrmnon@gmail.com>
2018-08-01 01:08:41 +0400

    #28: Modified Schedule, Repo and Service.

--- back-end/src/main/java/com/javabrazzers/domain/Schedule.java
+++ back-end/src/main/java/com/javabrazzers/domain/Schedule.java
@@ -1,11 +1,27 @@
 package com.javabrazzers.domain;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.Entity;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
 import java.time.LocalDateTime;
 
-public class Schedule {
+@Getter
+@Setter
+@Entity
+@Table(name = "schedule_tbl")
+@AllArgsConstructor
+@NoArgsConstructor
+public class Schedule extends BaseEntity {
 
-	private LocalDateTime depatureDate;
+	private LocalDateTime departureDate;
 	private BusRoute busRoute;
+
+	@OneToMany
 	private Bus Bus;
 
 }
---
+++ back-end/src/main/java/com/javabrazzers/repository/ScheduleRepositories.java
@@ -0,0 +1,7 @@
+package com.javabrazzers.repository;
+
+import com.javabrazzers.domain.Schedule;
+import com.javabrazzers.repository.common.BaseRepository;
+
+public interface ScheduleRepositories extends BaseRepository<Schedule, Long> {
+}
