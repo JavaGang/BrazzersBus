c2eade28886ef9cdc8c6ea90f47bd78506fde484
iBublig <theaganrmnon@gmail.com>
2018-08-01 00:01:40 +0400

    #28: Modified BusRoute, Repo and Service. This commit requires special attention

--- back-end/src/main/java/com/javabrazzers/domain/BusRoute.java
+++ back-end/src/main/java/com/javabrazzers/domain/BusRoute.java
@@ -5,23 +5,33 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
-import javax.persistence.Entity;
-import javax.persistence.Table;
+import javax.persistence.*;
+import javax.validation.constraints.Size;
 import java.time.LocalDateTime;
 
 
 @Getter
 @Setter
 @Entity
-@Table(name = "bus_tbl")
+@Table(name = "bus_route_tbl")
 @AllArgsConstructor
 @NoArgsConstructor
 public class BusRoute extends BaseEntity {
 
-	private LocalDateTime depatureTime;
-	private LocalDateTime arrivalTime;
+    @Column(name = "departure_time", nullable = false, length = 7, unique = false)
+    private LocalDateTime departureTime;
+
+    @Column(name = "arrival_time", nullable = false, length = 7, unique = false)
+    private LocalDateTime arrivalTime;
+
+    @Size(min = 1, max = 100, message = "errors.busflight.km.max")
+    @Column(name = "km", nullable = true, length = 10, unique = false)
 	private Integer km;
+
+
+	@ManyToOne
 	private BusFlight busFlight;
-	private BusStation busStatiom;
+	@OneToMany
+	private BusStation busStation;
 
 }
---
+++ back-end/src/main/java/com/javabrazzers/repository/BusRouteRepository.java
@@ -0,0 +1,7 @@
+package com.javabrazzers.repository;
+
+import com.javabrazzers.domain.BusRoute;
+import com.javabrazzers.repository.common.BaseRepository;
+
+public interface BusRouteRepository extends BaseRepository<BusRoute, Long> {
+}
